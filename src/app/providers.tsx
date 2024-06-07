eb8cb787058ebbf462cb27f1299605ba6f502ae7
Guilherme Azevedo <gui-azesantos@hotmail.com>
2024-06-07 16:37:13 -0300

    fix(build): fixing provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { NextUIProvider } from '@nextui-org/react';
-import { SessionProvider } from 'next-auth/react';
+import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -9,7 +9,7 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <SessionProvider>
+    <SessionProvider basePath="/api/auth">
       <NextUIProvider>{children}</NextUIProvider>
     </SessionProvider>
   );
